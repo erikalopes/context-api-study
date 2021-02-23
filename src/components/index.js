b05adb8a7d6c64a8f9532d952f46d98b2f27870e
erikalopes <201810099@acad.unijuazeiro.edu.br>
2021-02-23 18:46:27 -0300

    add confirm delete

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { View } from 'react-native'
 import { Button, Icon } from 'react-native-elements'
 import styled from 'styled-components'
 
-const IconAdd = () => <Icon name='add' size={25} color='#fff' />
+export const IconAdd = () => <Icon name='add' size={25} color='#fff' />
+
+export const IconEditUser = () => <Icon name='edit' size={25} color='black' />
+export const IconDeleteUser = () => <Icon name='delete' size={25} color='red' />
 
 export const StyledButton = styled(Button).attrs({
   type: 'clear',
   icon: <IconAdd />,
 })``
+
+export const GroupButtonActions = styled(View)`
+  flex-direction: row;
+  align-self: flex-end;
+`
--- src/screens/Users/List.js
+++ src/screens/Users/List.js
@@ -1,9 +1,45 @@
 import * as React from 'react'
-import { FlatList } from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import { Alert, FlatList, Text } from 'react-native'
+import { ListItem, Avatar, Button } from 'react-native-elements'
+import {
+  IconEditUser,
+  IconDeleteUser,
+  GroupButtonActions,
+} from '../../components'
 import { users } from '../../data/users'
 
 const ListUsers = (props) => {
+  function confirmDelete(user) {
+    Alert.alert('Exclusão', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete')
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ])
+  }
+
+  function getActions(item) {
+    return (
+      <GroupButtonActions>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', item)}
+          type='clear'
+          icon={<IconEditUser />}
+        />
+        <Button
+          onPress={() => confirmDelete(item)}
+          type='clear'
+          icon={<IconDeleteUser />}
+        />
+      </GroupButtonActions>
+    )
+  }
+
   function getUsers({ item }) {
     return (
       <ListItem bottomDivider>
@@ -12,7 +48,7 @@ const ListUsers = (props) => {
           <ListItem.Title>{item.name}</ListItem.Title>
           <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
         </ListItem.Content>
-        <ListItem.Chevron />
+        <ListItem.Content>{getActions(item.id)}</ListItem.Content>
       </ListItem>
     )
   }
