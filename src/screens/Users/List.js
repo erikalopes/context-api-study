fe3bad5efd72332776694e262e4988a69279236f
erikalopes <201810099@acad.unijuazeiro.edu.br>
2021-02-23 18:20:52 -0300

    add list

--- src/screens/Users/List.js
+++ src/screens/Users/List.js
@@ -1,7 +1,30 @@
 import * as React from 'react'
-import { Text } from 'react-native'
+import { FlatList } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+import { users } from '../../data/users'
 
 const ListUsers = (props) => {
-  return <Text>List Users</Text>
+  function getUsers({ item }) {
+    return (
+      <ListItem bottomDivider>
+        <Avatar source={{ uri: item.avatar }} />
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
+    )
+  }
+
+  return (
+    <>
+      <FlatList
+        data={users}
+        keyExtractor={(user) => user.id.toString()}
+        renderItem={getUsers}
+      />
+    </>
+  )
 }
 export default ListUsers
